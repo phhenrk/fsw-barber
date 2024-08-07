adccc5a29c651de7ec780e2229bfd453cbcb51c4
phhenrk <phhenrk3@gmail.com>
2024-08-07 12:02:07 -0300

    feat: add Header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,38 @@
 
+import { SearchIcon } from "lucide-react";
+import Header from "./_components/header";
 import { Button } from "./_components/ui/button";
-
+import { Input } from "./_components/ui/input";
+import Image from "next/image";
 
 const Home = () => {
- return <Button>teste</Button>
+    return (
+        <div>
+
+            <Header />
+
+            <div className="p-8">
+                <h2 className="text-xl font-bold">Olá, Felipe</h2>
+                <p> segunda-feira, 05 de agosto</p>
+                <div className="mt-6 flex items-center gap-2">
+                    <Input placeholder="Faça sua busca..." />
+                    <Button>
+                        <SearchIcon />
+                    </Button>
+                </div>
+
+                <div className="relative mt-6 h-[150px] w-full">
+
+                    <Image alt="banner capa principal" src="/banner-01.png" fill
+                     className="rounded-xl object-cover" />
+
+                </div>
+
+
+            </div>
+
+        </div>
+    )
 };
 
 
